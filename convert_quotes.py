a09f77cf461d72ff9b7ec73a8379b7ae96f92890
ik5 <idok@linesip.com>
2013-02-04 21:43:17 +0200

    Starting to move things into a class

--- convert_quotes.py
+++ convert_quotes.py
@@ -9,69 +9,99 @@ import fdb
 import logging
 import atexit
 
-def init_logger() :
+class Converter :
+
+    logger = None
+    con    = None
+
+    def __init__(self, a_logger, a_connection) :
+        logger    = logging.getLogger   ('convert_quotes')
+        handler   = logging.FileHandler ('log/convert.log')
+        formatter = logging.Formatter   (('[%(asctime)s - %(levelname)s] '
+                                         '%(message)s'))
+
+        handler.setFormatter (formatter    )
+        logger.addHandler    (handler      )
+        logger.setLevel      (logging.DEBUG)
+
+        con = a_connection
+
+    def parse(*a_list) :
+        quote  = []
+        author = ''
+        ref    = ''
+        a_list = []
+
+
+
+    def iterate_quote() :
+        full_quote = []
+
+        with open('quotes.txt') as a_file :
+            for line in a_file :
+                if line == '----\n' :
 #
-# readability over everything else !
-# http://www.python.org/dev/peps/pep-0008/#pet-peeves <- Really ? 
-# I'm an Object Pascal developer, and that's how we do things, 
-# even our editors/ide support this :P
+                    self.parse(full_quote)
+                    full_quote = []
+                else :
+                    full_quote.append(line)
+
+
+#def init_logger() :
+##
+## readability over everything else !
+## http://www.python.org/dev/peps/pep-0008/#pet-peeves <- Really ? 
+## I'm an Object Pascal developer, and that's how we do things, 
+## even our editors/ide support this :P
+##
 #
-    logger    = logging.getLogger   ('convert_quotes')
-    handler   = logging.FileHandler ('log/convert.log')
-    formatter = logging.Formatter   (('[%(asctime)s - %(levelname)s] '
-                                     '%(message)s'))
-
-    handler.setFormatter (formatter    )
-    logger.addHandler    (handler      )
-    logger.setLevel      (logging.DEBUG)
-
-    return logger
-
-def finalize(db_connection) :
-    if not db_connection.closed :
-        db_connection.close
-
-def parse_list(a_list, con, logger) :
-    if not a_list :
-       return None
-
-    return None
-
-logger = init_logger()
-logger.info('Entering convert_quotes.py')
-
-
-if not os.path.exists('quotes.txt') :
-    logger.critical('The file quotes.txt was not found')
-    sys.exit('Error: Could not find the quotes.txt file, bye')
-
-try :
-    con = fdb.connect(
-                       dsn      = '127.0.0.1:quotes',
-                       user     = 'sysdba', 
-                       password = 'masterkey'
-                     )
-
-    logger.info('Connected to the database')
-
-except fdb.DatabaseError as e :
-    logger.critical('Database connection error: %s', e)
-    sys.exit       ('Database error: {0}'.format(e))
-
-atexit.register(finalize, con)
-
-with open('quotes.txt') as a_file :
-    full_quote = []
-
-    for line in a_file :
-        if line == '----\n' :
-            # 
-            #
-            
-            full_quote = []
-        else :
-            full_quote.append(line)
-            
-
-
-
+#    return logger
+#
+#def finalize(db_connection) :
+#    if not db_connection.closed :
+#        db_connection.close
+#
+#def parse_list(a_list, con, logger) :
+#    if not a_list :
+#       return None
+#
+#    return None
+#
+#logger = init_logger()
+#logger.info('Entering convert_quotes.py')
+#
+#
+#if not os.path.exists('quotes.txt') :
+#    logger.critical('The file quotes.txt was not found')
+#    sys.exit('Error: Could not find the quotes.txt file, bye')
+#
+#try :
+#    con = fdb.connect(
+#                       dsn      = '127.0.0.1:quotes',
+#                       user     = 'sysdba', 
+#                       password = 'masterkey'
+#                     )
+#
+#    logger.info('Connected to the database')
+#
+#except fdb.DatabaseError as e :
+#    logger.critical('Database connection error: %s', e)
+#    sys.exit       ('Database error: {0}'.format(e))
+#
+#atexit.register(finalize, con)
+#
+#with open('quotes.txt') as a_file :
+#    full_quote = []
+#
+#    for line in a_file :
+#        if line == '----\n' :
+#            # 
+#            #
+#            
+#            full_quote = []
+#        else :
+#            full_quote.append(line)
+#            
+#
+#
+#
