911af7d85e0f90db180019b4855c13e47371c5e3
ik5 <idok@linesip.com>
2013-02-04 20:15:17 +0200

    Added atexit code to close the database connection, and now starting to work on the actual code

--- convert_quotes.py
+++ convert_quotes.py
@@ -7,10 +7,13 @@ import os
 import sys
 import fdb
 import logging
+import atexit
 
 #
-# readability over everything else ;)
-# Hey, I'm an Object Pascal developer, and that's how we do things :P
+# readability over everything else !
+# http://www.python.org/dev/peps/pep-0008/#pet-peeves <- Really ? 
+# I'm an Object Pascal developer, and that's how we do things, 
+# even our editors/ide support this :P
 #
 logger    = logging.getLogger   ('convert_quotes'                           )
 handler   = logging.FileHandler ('log/convert.log'                          )
@@ -21,6 +24,10 @@ logger.addHandler    (handler      )
 logger.setLevel      (logging.DEBUG)
 
 logger.info('entering convert_quotes.py')
+
+def finalize(db_connection) :
+    if not db_connection.closed :
+        db_connection.close
 
 if not os.path.exists('quotes.txt') :
     logger.critical('The file quotes.txt was not found')
@@ -38,5 +45,21 @@ try :
 except fdb.DatabaseError as e :
     logger.critical('Database connection error: %s', e)
     sys.exit       ('Database error: {0}'.format(e))
+
+atexit.register(finalize, con)
+
+with open('quotes.txt') as a_file :
+    full_quote = []
+
+    for line in a_file :
+        if line == '----\n' :
+            # 
+            #
+
+            full_quote = []
+        else :
+            full_quote.append(line)
+            
+
 
 
