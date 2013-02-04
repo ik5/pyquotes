6d9f23e6e986150afea6cd4ee3bc6d1ce4282f51
ik5 <idok@linesip.com>
2013-02-04 19:00:25 +0200

    Added support for logger

--- convert_quotes.py
+++ convert_quotes.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python2
+#
+#
+#
 
 import os
 import sys
 import fdb
+import logging
+
+#
+# readability over everything else ;)
+# Hey, I'm an Object Pascal developer, and that's how we do things :P
+#
+logger    = logging.getLogger   ('convert_quotes'                           )
+handler   = logging.FileHandler ('log/convert.log'                          )
+formatter = logging.Formatter   ('[%(asctime)s - %(levelname)s] %(message)s')
+
+handler.setFormtter (formatter    )
+logger.addHandler   (hadler       ) 
+logger.setLevel     (logging.DEBUG)
+
 
 if not os.path.exists('quotes.txt') :
     sys.exit('Error: Could not find the quotes.txt file, bye')
@@ -15,6 +32,4 @@ try :
                      )
 except fdb.DatabaseError as e :
     sys.exit("Database error: {0}".format(e))
-
-
 
