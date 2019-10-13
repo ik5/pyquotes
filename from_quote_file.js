6aa7c607ed8f0dd37f0f5d432783e592a8f51c7d
Ido Kanner <idok@linesip.com>
2019-10-13 19:24:11 +0300

    creating js web based quotes reading

---
+++ from_quote_file.js
@@ -0,0 +1,49 @@
+const quoteSeperator = '^----$';
+const authorSeperator = '^\s{4}(.*?)$';
+const quoteFileAddress = '/quotes.txt';
+
+const quoteBodyHTMLElement = 'quote-body';
+const fromHTMLElement = 'from';
+
+
+class Quotes {
+  htmlFields = { quoteBody: null, from: null };
+
+  constructor() {
+    this.htmlFields.quoteBody = document.getElementById(quoteBodyHTMLElement);
+    this.htmlFields.from = document.getElementById(fromHTMLElement);
+
+    this.getFile();
+  }
+
+  async getFile() {
+    let content;
+    const utf8Decoder = new TextDecoder('utf-8');
+    try {
+      const response = await window.fetch(quoteFileAddress);
+      const reader = response.body.getReader();
+      let { value: chunk, done: readerDone } = await reader.read();
+      chunk = chunk ? utf8Decoder.decode(chunk) : '';
+      content = chunk;
+    } catch (e) {
+      alert("Unable to get quote file: " + e.message);
+      return
+    }
+
+    this.parseContent(content);
+  }
+
+
+
+  parseContent(content) {
+
+  }
+
+  run() {
+
+  }
+}
+
+const quote = new Quotes();
+
+quote.run();
