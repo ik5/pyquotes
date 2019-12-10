827925790ed18db52c1f692d5f1cd083b03c8917
Ido Kanner <idok@linesip.com>
2019-12-10 17:36:45 +0200

    push current quote to url

--- from_quote_file.js
+++ from_quote_file.js
@@ -29,17 +29,17 @@ class Quotes {
     this.getFile();
   }
 
-   getFile() {
+  getFile() {
 
-     fetch(quoteFileAddress)
-     .then(response => {
-       response.text().then(
-         content => this.parseContent(content)
-       )
-     })
-     .catch(e => alert("Unable to get quote file: " + e));
+    fetch(quoteFileAddress)
+    .then(response => {
+      response.text().then(
+        content => this.parseContent(content)
+      )
+    })
+    .catch(e => alert("Unable to get quote file: " + e));
 
-    }
+  }
 
   parseContent(content) {
     const lines = content.split(quoteSeperator);
@@ -63,7 +63,8 @@ class Quotes {
     });
 
     this.htmlFields.total.innerText = `${this.quotesContainer.length}`;
-    this.getRandomQuote();
+
+    this.getQuote();
   }
 
   getRandomInt(max) {
@@ -86,9 +87,23 @@ class Quotes {
     return this.quotesContainer[this.quoteIndex];
   }
 
-  getRandomQuote() {
-    this.quoteIndex = this.getRandomInt(this.quotesContainer.length);
+  getQuote() {
+    let quoteNumber = null
+    if (this.url.hash != '') {
+      quoteNumber = parseInt(this.url.hash, 10);
+    } else if (this.url.args.length > 0) {
+      this.url.args.forEach( (value, name) => {
+        console.log(name, value);
+      } );
+    }
 
+    if (quoteNumber === null ||
+        isNaN(quoteNumber) ||
+          quoteNumber >= this.quotesContainer.length) {
+      this.getRandomQuote();
+    } else {
+        this.quoteNumber = quoteNumber;
+    }
     const quote = this.quotesContainer[this.quoteIndex];
     this.htmlFields.quoteBody.innerText = quote.text;
     this.htmlFields.quoteBody.style.fontSize = this.calcFontSize(quote.text.length);
@@ -100,12 +115,18 @@ class Quotes {
     }
 
     this.htmlFields.currentQuoteIndex.innerText = `${this.quoteIndex || 0}`;
+    window.history.pushState({quote: this.quoteNumber}, document.head.title,
+                             `?quote=${this.quoteNumber}`);
+  }
+
+  getRandomQuote() {
+    this.quoteIndex = this.getRandomInt(this.quotesContainer.length);
   }
 
   parseUrl(urlString) {
     let url = new URL(urlString || window.location.href);
     if (url.hash != '') {
-      this.url.hash = url.hash;
+      this.url.hash = url.hash.replace('#', '');
     }
 
     url.searchParams.forEach((value, key) => this.url.args.push({ key, value }));
